fe0f1ad756dd0ad7edf9411eeaee8d9a68d8fc90
mhartley19 <mhartley19@gmail.com>
2022-01-07 10:11:09 -0600

    added bootstrap component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Navbar} from './navbar'
+import {CustomCard} from './bootstrap-card'
 import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -8,15 +9,26 @@ export const Projects = () => {
         <div>
             <Navbar/>
             <div id="projects-container">
+            
+
+                <a href="https://mhartley19.github.io/blackjack-react/">
+                    <img class='projectimage bjimage' 
+                    src={require('../images/bj.png')} 
+                    alt='Black Jack game'/>
+
+                </a>
+                <br></br>
+                <CustomCard title="BlackJack" sub='Tech Stack: React' des='description working'/>
+                
                 <h3>Rock Paper Scissors</h3>
             
                 <a href="https://mhartley19.github.io/rock-paper-scissors/">
-                <img class='projectimage RPSimage' 
-                    src={require('../images/RPS.jpeg')} 
-                    alt='Rock Paper Scissors Game'/>
-                
-                
+                        <img class='projectimage RPSimage' 
+                        src={require('../images/RPS.jpeg')} 
+                        alt='Rock Paper Scissors Game'/>
                 </a>
+
+                
 
 
             
