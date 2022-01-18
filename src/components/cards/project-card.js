2a549ba8c9fe9499df2520c8c8988a82edeb6d9b
mhartley19 <mhartley19@gmail.com>
2022-01-18 13:44:27 -0600

    working on modal

--- src/components/cards/project-card.js
+++ src/components/cards/project-card.js
@@ -7,29 +7,40 @@ import {imageSRC} from '../../images/index'
 
 export const ProjectCard = (props) => {
 
+  let modalButtonText = "Open"
+  
+  function modalButton () {
+    let modal = document.getElementById('PC-modal-closed')
+    console.log(modal)
+    modal.id = 'PC-modal-open'
+    modalButtonText = "Close"
+    console.log(modalButtonText)
+  }
   
 
     return(
       <div id='PC'>
-        <header class='PC-header'>
+        <div class='PC-IM'>
         <a href={props.href}>
                           <img class={props.class}
                             src={props.src}
                             alt={props.alt}/>
                         </a>
-            <div class='PC-title-container'>
-              <h3 class='PC-title'>{props.title}</h3>
-            </div>
-        </header>
-            <p class='PC-subtitle'><b>{props.sub}</b></p>
-        <body>
-            <p class='PC-body'>{props.des}
-            </p>
-        </body>
-        <footer >
-          <a href={props.gitlink} class='PC-gitlink'>GitHub</a>
-
-        </footer>
+        </div>
+        <div id='PC-modal-closed'>
+          <header>
+              <h3 class='PC-modal-title'>{props.title}</h3>
+              <p class='PC-modal-subtitle'><b>{props.sub}</b></p>
+          </header>
+          <body>
+              <p class='PC-modal-body'>{props.des}
+              </p>
+          </body>
+          <footer class='PC-modal-footer'>
+            <a href={props.gitlink} class='PC-gitlink'>GitHub</a>
+          </footer>
+        </div>
+          <button onClick={modalButton}>{modalButtonText}</button>
       </div>
     )
 }
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -21,60 +21,49 @@ export const Projects = () => {
         <div id='PP'> 
             <Navbar/>
             <div class="PP-allprojects-container">
-        
-
+                
                 {/* Project Card One */}
 
                 <div class='PP-container-one PP-containers'>
-     
-                        <ProjectCard 
-                                    href='https://mhartley19.github.io/blackjack-react/'
-                                    alt='Black Jack game'
-                                    src={blackJackImage}
-                                    class = "PP-IM IM-blackjack"
-                                    title="Black Jack" 
-                                    sub='Tech Stack: React, JavaScript' 
-                                    des='Black Jack game created with React, includes double down, 
-                                        correct Ace value based on game situation'
-                                    gitlink='https://mhartley19.github.io/rock-paper-scissors/'/>
+                    <ProjectCard 
+                        href='https://mhartley19.github.io/blackjack-react/'
+                        alt='Black Jack game'
+                        src={blackJackImage}
+                        class = "PP-IM IM-blackjack"
+                        title="Black Jack" 
+                        sub='Tech Stack: React, JavaScript' 
+                        des='Black Jack game created with React, includes double down, 
+                            correct Ace value based on game situation'
+                        gitlink='https://mhartley19.github.io/rock-paper-scissors/'/>
                          </div>  
 
-                
                 {/* Project Card Two */}  
-                
+
                 <div class='PP-container-two PP-containers'>
-                        <ProjectCard 
-                                    alt='Twitter Clone Image' 
-                                    src={twitterImage} 
-                                    sub="Tech Stack: Python"
-                                    title='Twitter Clone'
-                                    class ='PP-IM IM-twitter-clone'  
-                                    des='This is a clone of Twitter using Python, it utilizes Models and Views.
-                                    It featues user creation, user login, "tweets" direct messaging and notifications
-                                    ' 
-                                    gitlink="https://github.com/kenzie-se-q4/twitterclone-mhartley19"/>
-                        
+                    <ProjectCard 
+                        alt='Twitter Clone Image' 
+                        src={twitterImage} 
+                        sub="Tech Stack: Python"
+                        title='Twitter Clone'
+                        class ='PP-IM IM-twitter-clone'  
+                        des='This is a clone of Twitter using Python, it utilizes Models and Views.
+                        It featues user creation, user login, "tweets" direct messaging and notifications
+                        ' 
+                        gitlink="https://github.com/kenzie-se-q4/twitterclone-mhartley19"/>
                 </div> 
 
-                {/* Project Card Three */}    
+                {/* Project Card Three */}  
+
                 <div class='PP-container-three PP-containers'>
-
-                
-                    
-                           
-                    
                     <ProjectCard title="Rock Paper Scissors" 
-                                href="https://mhartley19.github.io/rock-paper-scissors/"
-                                alt='Rock Paper Scissors Game'
-                                class = 'PP-IM IM-rps'
-                                src={rpsImage}
-                                sub='Tech Stack: Javascript, CSS, HTML' 
-                                des='Rock Paper Scissors was deisnged using JavaScript, 
-                                CSS and HTML, please click image for link to site'
-                                gitlink='https://github.com/mhartley19/rock-paper-scissors'/>
-
-                    
-                
+                        href="https://mhartley19.github.io/rock-paper-scissors/"
+                        alt='Rock Paper Scissors Game'
+                        class = 'PP-IM IM-rps'
+                        src={rpsImage}
+                        sub='Tech Stack: Javascript, CSS, HTML' 
+                        des='Rock Paper Scissors was deisnged using JavaScript, 
+                        CSS and HTML, please click image for link to site'
+                        gitlink='https://github.com/mhartley19/rock-paper-scissors'/>
                 </div>
             </div>
 
