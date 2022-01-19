0cb1130a388b09329de6da80a888c9bdac951663
mhartley19 <mhartley19@gmail.com>
2022-01-19 12:38:12 -0600

    updating cards, added box shadow, transition and hover effects

--- src/components/cards/project-card.js
+++ src/components/cards/project-card.js
@@ -2,45 +2,52 @@ import React from 'react'
 import {Card} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {imageSRC} from '../../images/index'
+import {useState} from 'react'
 
 
 
 export const ProjectCard = (props) => {
 
-  let modalButtonText = "Open"
-  
-  function modalButton () {
-    let modal = document.getElementById('PC-modal-closed')
-    console.log(modal)
-    modal.id = 'PC-modal-open'
-    modalButtonText = "Close"
-    console.log(modalButtonText)
-  }
+  const [modalButtonText, setModalButtonText] = useState("Open")
   
 
+
     return(
-      <div id='PC'>
-        <div class='PC-IM'>
-        <a href={props.href}>
-                          <img class={props.class}
-                            src={props.src}
-                            alt={props.alt}/>
-                        </a>
+      // <div id='PC'>
+        <div class='PC'>
+            <img class={props.imageClass}
+                        src={props.imageSrc}
+                        alt={props.imageAlt}/>
+          <div class="PC-buttons">
+            <a href={props.linkButtonHref}>
+              <button id={props.linkButtonID}
+                      class={props.buttonClass}>
+                      Link
+              </button>
+            </a>
+              <button id={props.modalButtonID}
+                      class={props.buttonClass}
+                  >
+                      Modal
+              </button>
+          </div>
         </div>
-        <div id='PC-modal-closed'>
-          <header>
-              <h3 class='PC-modal-title'>{props.title}</h3>
-              <p class='PC-modal-subtitle'><b>{props.sub}</b></p>
-          </header>
-          <body>
-              <p class='PC-modal-body'>{props.des}
-              </p>
-          </body>
-          <footer class='PC-modal-footer'>
-            <a href={props.gitlink} class='PC-gitlink'>GitHub</a>
-          </footer>
-        </div>
-          <button onClick={modalButton}>{modalButtonText}</button>
-      </div>
+
+
+
+        // <div id='PC-modal-closed'>
+        //   <header>
+        //       <h3 class='PC-modal-title'>{props.title}</h3>
+        //       <p class='PC-modal-subtitle'><b>{props.sub}</b></p>
+        //   </header>
+        //   <body>
+        //       <p class='PC-modal-body'>{props.des}
+        //       </p>
+        //   </body>
+        //   <footer class='PC-modal-footer'>
+        //     <a href={props.gitLink} class='PC-gitlink'>GitHub</a>
+        //   </footer>
+        // </div>
+      // </div>
     )
 }
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -24,47 +24,56 @@ export const Projects = () => {
                 
                 {/* Project Card One */}
 
-                <div class='PP-container-one PP-containers'>
+                <div class='PC-container-one PC-container'>
                     <ProjectCard 
-                        href='https://mhartley19.github.io/blackjack-react/'
-                        alt='Black Jack game'
-                        src={blackJackImage}
-                        class = "PP-IM IM-blackjack"
+                        imageAlt='Black Jack game'
+                        imageSrc={blackJackImage}
+                        imageClass = "PP-IM IM-blackjack"
+                        linkButtonHref='https://mhartley19.github.io/blackjack-react/'
+                        linkButtonID = "Sitelink-BlackJack"
+                        modalButtonID= "Modal-BlackJack"
+                        buttonClass = "sitelink"
                         title="Black Jack" 
                         sub='Tech Stack: React, JavaScript' 
                         des='Black Jack game created with React, includes double down, 
                             correct Ace value based on game situation'
-                        gitlink='https://mhartley19.github.io/rock-paper-scissors/'/>
-                         </div>  
+                        gitLink='https://mhartley19.github.io/rock-paper-scissors/'/>
+ 
+                    <ProjectCard 
+                        imageAlt='Twitter Clone Site'
+                        imageSrc={twitterImage}
+                        imageClass = "PP-IM IM-Twitter"
+                        modalButtonID= "Modal-BlackJack"
+                        buttonClass = "sitelink"
+                        title="Black Jack" 
+                        sub='Tech Stack: React, JavaScript' 
+                        des='Black Jack game created with React, includes double down, 
+                            correct Ace value based on game situation'
+                        gitLink='https://mhartley19.github.io/rock-paper-scissors/'/>
+                    <ProjectCard 
+                        imageAlt='Rock Paper Scissors game'
+                        imageSrc={rpsImage}
+                        imageClass = "PP-IM IM-Rock-Paper-Scissors"
+                        linkButtonHref="https://mhartley19.github.io/rock-paper-scissors/"
+                        linkButtonID = "Sitelink-BlackJack"
+                        modalButtonID= "Modal-BlackJack"
+                        buttonClass = "sitelink"
+                        title="Black Jack" 
+                        sub='Tech Stack: React, JavaScript' 
+                        des='Black Jack game created with React, includes double down, 
+                            correct Ace value based on game situation'
+                        gitLink='https://mhartley19.github.io/rock-paper-scissors/'/>
+
+                 
 
                 {/* Project Card Two */}  
 
-                <div class='PP-container-two PP-containers'>
-                    <ProjectCard 
-                        alt='Twitter Clone Image' 
-                        src={twitterImage} 
-                        sub="Tech Stack: Python"
-                        title='Twitter Clone'
-                        class ='PP-IM IM-twitter-clone'  
-                        des='This is a clone of Twitter using Python, it utilizes Models and Views.
-                        It featues user creation, user login, "tweets" direct messaging and notifications
-                        ' 
-                        gitlink="https://github.com/kenzie-se-q4/twitterclone-mhartley19"/>
-                </div> 
+
 
                 {/* Project Card Three */}  
 
-                <div class='PP-container-three PP-containers'>
-                    <ProjectCard title="Rock Paper Scissors" 
-                        href="https://mhartley19.github.io/rock-paper-scissors/"
-                        alt='Rock Paper Scissors Game'
-                        class = 'PP-IM IM-rps'
-                        src={rpsImage}
-                        sub='Tech Stack: Javascript, CSS, HTML' 
-                        des='Rock Paper Scissors was deisnged using JavaScript, 
-                        CSS and HTML, please click image for link to site'
-                        gitlink='https://github.com/mhartley19/rock-paper-scissors'/>
-                </div>
+
+                </div> 
             </div>
 
         </div>   
