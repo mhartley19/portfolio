29a59a680ca229682a9220dd926aae34ac61352f
mhartley19 <mhartley19@gmail.com>
2022-01-19 12:37:06 -0600

    possible file for modal

---
+++ src/components/cards/modal-card.js
@@ -0,0 +1,7 @@
+function modalButton () {
+    let modal = document.getElementById('PC-modal-closed')
+    console.log(modal)
+    modal.id = 'PC-modal-open'
+    setModalButtonText('close')
+    console.log(modalButtonText)
+  }
